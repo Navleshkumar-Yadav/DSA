179551aa120fb8e47dc32260bedabd8566e5436c
Navlesh Yadav <119070031+Navleshkumar-Yadav@users.noreply.github.com>
2024-11-08 00:30:17 +0530

    Update 00.cpp

--- 00 Basics/00.cpp
+++ 00 Basics/00.cpp
@@ -1,25 +1,25 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//         cout<<"Ram";
-// }
+#include<iostream>
+using namespace std;
+int main(){
+        cout<<"Ram";
+}
 
 
-// #include<iostream>
-// using namespace std;
-// int main(){
-//         int n,m;
-//         cout<<"Enter the number: "<<endl;
-//         cin>>n>>m;
-//         for(int i = 1; i<n ; i++){
-//                 for(int j =  1; j<m ; j++){
-//                         cout<<"*";
-//                 }
-//                 cout<<endl;
+#include<iostream>
+using namespace std;
+int main(){
+        int n,m;
+        cout<<"Enter the number: "<<endl;
+        cin>>n>>m;
+        for(int i = 1; i<n ; i++){
+                for(int j =  1; j<m ; j++){
+                        cout<<"*";
+                }
+                cout<<endl;
 
-//         }
+        }
 
-// }
+}
 
 
 #include<iostream>
