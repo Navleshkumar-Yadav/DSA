b39c867cf0dd4c5c3360e213f35cfa965ff27f26
Navlesh Yadav <119070031+Navleshkumar-Yadav@users.noreply.github.com>
2024-11-14 18:28:48 +0530

    Create Stack, Priorityqueue and queue in vector..cpp

---
+++ Stack, Priorityqueue and queue in vector..cpp
@@ -0,0 +1,123 @@
+#include<bits/stdc++.h>
+using namespace std;
+void explainStack(){
+    stack<int> st;
+    st.push(10);
+    st.push(50);
+    st.push(60);
+    st.push(70);
+   
+  while(st.empty() == false){
+      cout<<st.top()<<" ";
+      st.pop();
+      
+     
+     
+     
+       
+       
+  }
+    
+
+}
+int main(){
+    explainStack();
+
+
+    
+    
+}
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+void explainQueue(){
+    queue<int> q;
+    q.push(10);
+    q.push(50);
+    q.push(60);
+    q.push(70);
+   
+  while(q.empty() == false){
+      cout<<q.front()<<" ";
+      q.pop();
+      
+     
+     
+     
+       
+       
+  }
+    
+
+}
+int main(){
+    explainQueue();
+    
+}
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+void explainQueue(){
+    queue<int> q;
+    q.push(10);
+    q.push(50);
+    q.push(60);
+    q.push(70);
+   
+  while(q.empty() == false){
+      cout<<q.front()<<" ";
+      q.pop();
+      
+     
+     
+     
+       
+       
+  }
+    
+
+}
+int main(){
+    explainQueue();
+    
+}
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+void explainPriorityQueue(){
+    priority_queue<int> pq;
+    pq.push(100);
+    pq.push(50);
+    pq.push(60);
+    pq.push(70);
+   
+   while(pq.empty() == false){
+       cout<<pq.top()<<" ";
+       pq.pop();
+      
+     
+     
+     
+       
+       
+   }
+    
+
+}
+int main(){
+    explainPriorityQueue();
+    
+}
+
+
+
+
